75c8ad12f0103b7936b3aa3d5287474de29db350
bhanubais <bhanubais@gmail.com>
2021-03-28 12:49:35 +0530

    file/folder structure created

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,62 +1,26 @@
-const {parallel, series} = require('gulp');
+const {parallel, series, src, dest, watch} = require('gulp')
 
-
-function clean(cb) {
-	console.log("Task: clean");
-	cb();
+function html()
+{
+	return src('src/pages/**/*.html')
+	.pipe(panini({
+		root: 'pages/',
+		layouts: 'layouts/',
+		partials: 'partials/',
+		helpers: 'helpers/',
+		data: 'data/'
+	}))
+	.pipe(dest('public'));
 }
 
-function cssTranspile(cb) {
-	console.log("Task: cssTranspile");
-	cb();
-}
-
-function cssMinified(cb) {
-	console.log("Task: cssMinified");
-	cb();
-}
-
-function jsTranspile(cb) {
-	console.log("Task: jsTranspile");
-	cb();
-}
-
-function jsBundle(cb) {
-	console.log("Task: jsBundle");
-	cb();
-}
-
-function jsMinified(cb) {
-	console.log("Task: jsMinified");
-	cb();
-}
-
-function publish(cb) {
-	console.log("Task: publish");
-	cb();
-}
-
-function livereload(cb) {
-	console.log("Task: livereload");
+function panini_refresh(cb) {
+	panini.refresh();
 	cb();
 }
 
 
-if (process.env.NODE_ENV === 'production') {
-	exports.build = series(
-		clean,
-		parallel(cssTranspile,
-			series(jsTranspile, jsBundle)),
-		parallel(cssMinified, jsMinified),
-		publish
-	);
-} else {
-	exports.build = series(
-		clean,
-		parallel(cssTranspile,
-			series(jsTranspile, jsBundle)),
-		livereload
-	);
+exports.default = function(){
+	watch('src/pages/**/*.html', html)
+	watch(['src/**', '!src/assets/**', '!src/pages/**'], panini_refresh);
 }
-
 
